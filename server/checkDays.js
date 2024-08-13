55df36152c0c2c2b34d1f37a4ed9464738464c52
Sifr <sbyphone@disroot.org>
2024-08-13 13:10:33 +0100

    checkDays now returns an object which also provides the time in days since the last API call

--- server/checkDays.js
+++ server/checkDays.js
@@ -1,11 +1,6 @@
 // checkDays.js
 // API call rate limiting function
 
-// init sqlite db
-// const dbFile = "./server/.data/sqlite.db";
-// const sqlite3 = require("sqlite3").verbose();
-// const db = new sqlite3.Database(dbFile);
-
 // define function
 const checkDays = async (db, numberOfDays) => {
     return new Promise((resolve, reject) => {
@@ -16,12 +11,12 @@ const checkDays = async (db, numberOfDays) => {
                 return reject(err);
             }
             if (!rows || rows.length === 0) {
-                return resolve(true); // If there are no entries in the table, we can proceed with the API call
+                return resolve({ canProceed: true, daysDifference: null }); // If there are no entries in the table, we can proceed with the API call
             }
             const timeDifference = Date.now() - rows[0].timeStamp; // Difference between current time and the most recent timeStamp in the table
             const daysDifference = timeDifference / (1000 * 3600 * 24); // Convert difference from milliseconds to days
 
-            resolve(daysDifference > numberOfDays); // Return true if more than x number of days have passed
+            resolve({ canProceed: daysDifference > numberOfDays, daysDifference }); // Return an object with both values: Return true if more than x number of days have passed & number of days passed
         });
     });
 };
